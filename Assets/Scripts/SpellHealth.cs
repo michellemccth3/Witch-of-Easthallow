55f755181c47920a996789ba745825c127e0c170
Michelle <michellemccth3@gmail.com>
2023-10-13 18:46:37 -0400

    Added another sound effect

--- Assets/Scripts/SpellHealth.cs
+++ Assets/Scripts/SpellHealth.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SpellHealth : MonoBehaviour
 {
+    public AudioSource spellSound;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +21,9 @@ public class SpellHealth : MonoBehaviour
     {
         if(collision.gameObject.tag == "Monster")
         {
+            //spellSound.Play();
             Destroy(gameObject);
+            //spellSound.Play();
         }
     }
 }
