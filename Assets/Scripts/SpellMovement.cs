22c5a391f4fc781055500c299d8cbb2dae2a4e4d
Michelle <michellemccth3@gmail.com>
2023-10-13 17:01:18 -0400

    Fixed SpellMovement

--- Assets/Scripts/SpellMovement.cs
+++ Assets/Scripts/SpellMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnMovement : MonoBehaviour
+public class SpellMovement : MonoBehaviour
 {
     public GameObject spell1;
     // Start is called before the first frame update
